2c2776c6b76018d91687a5a0bac12dae40c14516
Andrew <andrew.devels@gmail.com>
2019-03-16 18:38:17 +0500

    feat(unsplash): update HTTP chained response handlers

--- Phaber.Unsplash/Http/ChainableValidationHandler.cs
+++ Phaber.Unsplash/Http/ChainableValidationHandler.cs
@@ -1,20 +1,57 @@
+using System;
 using System.Net.Http;
+using System.Linq;
 
 namespace Phaber.Unsplash.Http {
-    abstract public class ChainableValidationHandler : IValidatableHttpResponse {
-        protected IValidatableHttpResponse _nextHandler;
+    public sealed class ChainableValidationHandler : IValidatableHttpResponse {
+        private readonly IValidatableHttpResponse _handler;
+        private ChainableValidationHandler _chainedHandlers;
 
-        public ChainableValidationHandler(IValidatableHttpResponse handler = null) {
-            _nextHandler = handler;
+        public ChainableValidationHandler(
+            IValidatableHttpResponse handler,
+            params IValidatableHttpResponse[] handlers
+        ) {
+            _handler = handler;
+
+            if (handlers.Length > 0)
+                _chainedHandlers = new ChainableValidationHandler(
+                    handlers.First(),
+                    handlers.Take(handlers.Length - 1).ToArray()
+                );
         }
 
-        public abstract void Validate(HttpResponseMessage response);
+        /// <summary>
+        ///     Add <paramref name="handler"/>  to the chain
+        /// </summary>
+        /// <param name="handler">
+        ///     Will be the next handler in the chain
+        /// </param>
+        /// <returns>
+        ///     The next chain <paramref name="handler"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Throw if <paramref name="handler"/> is null
+        /// </exception>
+        /// <exception cref="InvalidOperationException">
+        ///     Throw if chain already has next handler
+        /// </exception>
+        public ChainableValidationHandler Add(IValidatableHttpResponse handler) {
+            if (handler == null)
+                throw new ArgumentNullException();
+
+            if (_chainedHandlers != null)
+                throw new InvalidOperationException();
+
+            _chainedHandlers = new ChainableValidationHandler(handler);
+
+            return _chainedHandlers;
+        }
 
         public void Handle(HttpResponseMessage response) {
-            Validate(response);
+            _handler.Handle(response);
 
-            if (_nextHandler != null)
-                _nextHandler.Handle(response);
+            if (_chainedHandlers != null)
+                _chainedHandlers.Handle(response);
         }
     }
 }
